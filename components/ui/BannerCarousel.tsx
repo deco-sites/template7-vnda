c804706df479db44bd61195013bc6430925e1640
jovenan <jovenan2001@gmail.com>
2023-03-27 22:00:25 -0300

    feat: carousel banner

--- components/ui/BannerCarousel.tsx
+++ components/ui/BannerCarousel.tsx
@@ -15,16 +15,7 @@ export interface Banner {
   mobile: LiveImage;
   /** @description Image's alt text */
   alt: string;
-  action?: {
-    /** @description when user clicks on the image, go to this link */
-    href: string;
-    /** @description Image text title */
-    title: string;
-    /** @description Image text subtitle */
-    subTitle: string;
-    /** @description Button label */
-    label: string;
-  };
+  href: string;
 }
 
 export interface Props {
@@ -45,19 +36,19 @@ function BannerItem({ image, lcp }: { image: Banner; lcp?: boolean }) {
     alt,
     mobile,
     desktop,
-    action,
+    href
   } = image;
 
   return (
-    <div class="relative h-[600px] min-w-[100vw] overflow-y-hidden">
-      <a href={action?.href ?? "#"} aria-label={action?.label}>
+    <div class="relative min-w-[100vw] overflow-y-hidden">
+      <a href={href ?? "#"} aria-label={href}>
         <Picture class="w-full" preload={lcp}>
           <Source
             media="(max-width: 767px)"
             fetchPriority={lcp ? "high" : "auto"}
             src={mobile}
-            width={360}
-            height={600}
+            width={414}
+            height={341}
           />
           <Source
             media="(min-width: 768px)"
@@ -67,26 +58,12 @@ function BannerItem({ image, lcp }: { image: Banner; lcp?: boolean }) {
             height={600}
           />
           <img
-            class="object-cover w-full sm:h-full"
+            class="w-full sm:h-full"
             loading={lcp ? "eager" : "lazy"}
             src={desktop}
             alt={alt}
           />
         </Picture>
-        {action && (
-          <div
-            class="absolute top-0 bottom-0 m-auto left-0 right-0 sm:right-auto sm:left-[12%] max-h-min max-w-[235px] flex flex-col gap-4 bg-hover-inverse p-4 rounded"
-            style={{ backdropFilter: "blur(8px)" }}
-          >
-            <Text variant="heading-1" tone="default-inverse">
-              {action.title}
-            </Text>
-            <Text variant="heading-3" tone="default-inverse">
-              {action.subTitle}
-            </Text>
-            <Button variant="secondary">{action.label}</Button>
-          </div>
-        )}
       </a>
     </div>
   );
@@ -184,7 +161,7 @@ function BannerCarousel({ images, preload, interval }: Props) {
   return (
     <div
       id={id}
-      class="grid grid-cols-[48px_1fr_48px] sm:grid-cols-[120px_1fr_120px] grid-rows-[1fr_48px_1fr_48px]"
+      class="grid grid-cols-[48px_1fr_48px] sm:grid-cols-[120px_1fr_120px] grid-rows-[1fr_48px_1fr_48px] pt-[70px] md:pt-[100px]"
     >
       <Slider class="col-span-full row-span-full scrollbar-none gap-6">
         {images?.map((image, index) => (
