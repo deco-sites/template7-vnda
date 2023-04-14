025cf978c944aa6fe97d492e747d3f07271919bb
jovenan <jovenan2001@gmail.com>
2023-04-13 23:19:54 -0300

    ajutes

--- components/ui/HeroBanner.tsx
+++ components/ui/HeroBanner.tsx
@@ -22,7 +22,7 @@ function DoubleBanner(props: Props) {
           class={`relative z-[-2] w-full ${props.overlayBanner ? 'opacity-60' : ''}`}
         />
         <div class="z-[-1] md:(absolute top-[10%] left-[50%] translate-x-[-50%] items-start) max-w-[1296px] h-full w-full flex flex-col justify-center items-center px-[32px] text-center">
-          <h3 class={`${props.description ? 'text-[.8rem]' : 'text-[1rem]'} text-[#312f4f] uppercase mt-[10px]`}>{props.title}</h3>
+          <h3 class={`${props.description ? 'text-[.9rem]' : 'text-[1rem]'} text-[#312f4f] uppercase mt-[10px]`}>{props.title}</h3>
           <h1 class={`${props.description ? 'text-[1.5rem]' : 'text-[2rem]'} text-[#312f4f] font-bold leading-10`}>{props.subTitle}</h1>
           <p class="text-[.857rem] text-[#312f4f] text-left max-w-[500px]">{props.description}</p>
         </div>
